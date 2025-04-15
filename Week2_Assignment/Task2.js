6cf373bec81f71da92150b7b5e15f4d6ebec9f44
nam59 <148378887+nam-bhutani@users.noreply.github.com>
2025-04-15 18:19:00 +0530

    Add files via upload

---
+++ Week2_Assignment/Task2.js
@@ -0,0 +1,11 @@
+// Task 2: Arithmetic Operations
+let x = 10;
+let y = 5;
+
+let sum = x + y;
+let product = x * y;
+let modulus = x % y;
+
+console.log("Sum:", sum);        // Output: Sum: 15
+console.log("Product:", product);  // Output: Product: 50
+console.log("Modulus:", modulus);   // Output: Modulus: 0
